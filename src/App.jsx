eac342e4ffd622eaa750151795c4d0f8f2747c7f
Kazi Foisal <kazimdfoisal7409@gmail.com>
2025-08-13 08:28:53 +0600

    Not found page added .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import ByCtegoryPage from './pages/ByCtegoryPage'
 import PorstDestailsPage from './pages/PorstDestailsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route path='/' element={<Home/>}></Route>
          <Route path='/byCategory/:categoryID' element={<ByCtegoryPage/>}></Route>
          <Route path='/postDetails/:postID' element={<PorstDestailsPage/>}></Route>
+         <Route path='*' element={<NotFoundPage/>}></Route>
         </Routes>
         
       </HashRouter>
--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,5 +1,4 @@
 
-
 const BlogDetails = (props) => {
   return (
     <>
